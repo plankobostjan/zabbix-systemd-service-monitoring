671d8763bce835b77549ec36b7a132e3f47c0a0b
Boštjan Planko <bostjan.planko@dhh.si>
2020-09-03 09:50:45 +0200

    removed service lists

--- usr/local/bin/zbx_service_discovery.sh
+++ usr/local/bin/zbx_service_discovery.sh
@@ -2,12 +2,10 @@
 
 service_list=$(systemctl list-unit-files | grep -E '\.service\s+(generated|enabled)' | awk -F'.service ' '{print $1}')
 
-[[ -r /etc/zabbix/service_discovery_whitelist ]] && {
-    service_list=$(echo "$service_list" | grep -E -f /etc/zabbix/service_discovery_whitelist)
-}
-
-[[ -r /etc/zabbix/service_discovery_blacklist ]] && {
-    service_list=$(echo "$service_list" | grep -Ev -f /etc/zabbix/service_discovery_blacklist)
-}
-
-echo -n '{"data":[';for s in ${service_list}; do echo -n "{\"{#SERVICE}\": \"$s\"},";done | sed -e 's:\},$:\}:';echo -n ']}'
+if [ $# -eq 0 ]
+then
+	echo -n '{"data":[';for s in ${service_list}; do echo -n "{\"{#SERVICE}\": \"$s\"},";done | sed -e 's:\},$:\}:';echo -n ']}'
+elif [ $1 = "critical" ]
+then
+	echo -n '{"data":[';for s in ${service_list}; do echo -n "{\"{#SERVICE.CRITICAL}\": \"$s\"},";done | sed -e 's:\},$:\}:';echo -n ']}'
+fi
